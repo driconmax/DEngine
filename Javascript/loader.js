ab784487fbf43d0ba9f3a739289df9dac3b24ce0
Driconmax <driconmax@gmail.com>
2017-05-08 12:06:18 -0300

    Changes in the loader script

--- Javascript/loader.js
+++ Javascript/loader.js
@@ -1,16 +1,23 @@
-var scripts = [
-    {
-        name: "MainScript",
-        url: "https://raw.githubusercontent.com/driconmax/dengine/master/Javascript/main.js"
+(function(){
+    var scripts = [
+        {
+            name: "MainScript",
+            url: "https://raw.githubusercontent.com/driconmax/dengine/master/Javascript/main.js"
+        }
+    ];
+    for (var i = 0; i < scripts.length; i++) {
+        try{
+            var item = scripts[i];
+            var x = document.createElement('script');
+            x.src = item.url;
+            var head = document.getElementsByTagName("head")[0];
+            if(head == null){
+                throw "The HTML tag HEAD is not defined";
+            } else {
+                appendChild(x);
+            }
+        } catch(e){
+            console.error("DEngine error: " + e);
+        }
     }
-];
-for (var i = 0; i < scripts.length; i++) {
-    try{
-        var item = scripts[i];
-        var x = document.createElement('script');
-        x.src = item.url;
-        document.getElementsByTagName("body")[0].appendChild(x);
-    } catch(e){
-        console.error("DEngine error: " + e);
-    }
-}
+})()
