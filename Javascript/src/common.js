43d7828a244bec0869a74afc1a3f82d2dec56bae
Driconmax <driconmax@gmail.com>
2017-06-02 10:11:15 -0300

    Updated common.js

--- Javascript/src/common.js
+++ Javascript/src/common.js
@@ -20,7 +20,9 @@
             var date = new Date();
             return {
                 TimeStamp: function(){
-                    return date.getHours()+":"+date.getMinutes()+":"+date.getSeconds();
+                    return  ((date.getHours() < 10)?"0":"")+date.getHours()+":" +
+                            ((date.getMinutes() < 10)?"0":"")+date.getMinutes()+":" +
+                            ((date.getSeconds() < 10)?"0":"")+date.getSeconds();
                 }
             }
         }
