64679c7ccb1ca230db582f323f51e77cd5ee966e
Driconmax <driconmax@gmail.com>
2017-06-22 16:21:18 -0300

    Added Base Class, Created Editor

---
+++ Javascript/src/base.js
@@ -0,0 +1,20 @@
+/* Navia Engine Base Class */
+
+class NaviaBase {
+    
+    constructor(){
+        super();
+        this.modules = [];
+    }
+    
+    //Navia Starts here
+    start() {
+        
+    }
+    
+    //Called once each frame
+    update(obj){
+        
+    }
+    
+}
---
+++ Javascript/tools/editor/js/editor.js
@@ -0,0 +1,26 @@
+/* Navia Engine Editor */
+
+window.naviaEditor = new class NaviaEditor extends NaviaBase {
+    
+    constructor(){
+        super();
+        this.modules = [];
+    }
+    
+    //Navia Starts here
+    start() {
+        $e.setBackground("#000");
+    }
+    
+    //Called once each frame
+    update(obj){
+        for(var i = 0; i < this.modules.length; i++){
+            
+        }
+    }
+    
+    addModule(path){
+        
+    }
+    
+}
