66dbf8f939667a8ef929a4c583f4cfa9a67fcb02
driconmax <driconmax@gmail.com>
2017-07-20 00:22:21 -0300

    Zoom testing

--- Javascript/src/core.js
+++ Javascript/src/core.js
@@ -999,14 +999,14 @@ Collision Response - http://elancev.name/oliver/2D%20polygon.htm
                     internal.ctx.shadowColor = "#3c84c1";
                 }
                 if(obj.texture != undefined){
-                    internal.ctx.drawImage(obj.texture.getTexture(), (-obj.texture.size.x/2) * internal.camera.zoom, (-obj.texture.size.y/2)  * internal.camera.zoom, (obj.texture.size.x) * internal.camera.zoom, (obj.texture.size.y) * internal.camera.zoom);
+                    internal.ctx.drawImage(obj.texture.getTexture(), -obj.texture.size.x/2, -obj.texture.size.y/2, obj.texture.size.x, obj.texture.size.y);
                 } else {
                     if(obj.collider.type == 1){
                         //internal.ctx.arc(0, 0, obj.collider.radius/4, 0, 2*Math.PI);
                         //internal.ctx.fill();
-                        internal.ctx.fillRect(- 10/2 * internal.camera.zoom, - 10/2 * internal.camera.zoom, 10 * internal.camera.zoom, 10 * internal.camera.zoom);
+                        internal.ctx.fillRect(- 10/2, - 10/2, 10, 10);
                     } else {
-                        internal.ctx.fillRect(- 10/2 * internal.camera.zoom, - 10/2 * internal.camera.zoom, 10 * internal.camera.zoom, 10 * internal.camera.zoom);
+                        internal.ctx.fillRect(- 10/2, - 10/2, 10, 10);
                     }
                 }
 
@@ -1022,9 +1022,9 @@ Collision Response - http://elancev.name/oliver/2D%20polygon.htm
                         if(obj.collider.type == 1){
                             internal.ctx.arc(0, 0, obj.collider.radius, 0, 2*Math.PI);
                         } else {
-                            internal.ctx.moveTo(obj.collider.vertexs[obj.collider.vertexs.length-1].x * internal.camera.zoom, obj.collider.vertexs[obj.collider.vertexs.length-1].y * internal.camera.zoom);
+                            internal.ctx.moveTo(obj.collider.vertexs[obj.collider.vertexs.length-1].x, obj.collider.vertexs[obj.collider.vertexs.length-1].y);
                             for(var i = 0; i < obj.collider.vertexs.length; i++){
-                                internal.ctx.lineTo(obj.collider.vertexs[i].x * internal.camera.zoom, obj.collider.vertexs[i].y * internal.camera.zoom);
+                                internal.ctx.lineTo(obj.collider.vertexs[i].x, obj.collider.vertexs[i].y);
                             }
                         }
                         //internal.ctx.strokeStyle = '#0F4';
@@ -1033,7 +1033,7 @@ Collision Response - http://elancev.name/oliver/2D%20polygon.htm
                         internal.ctx.stroke();
                         internal.ctx.fillStyle = "#FA0";
                         if(obj.collider.contactPoint != undefined){
-                            internal.ctx.fillRect((obj.collider.vertexs[obj.collider.contactPoint].x - 2) * internal.camera.zoom, ((obj.collider.vertexs[obj.collider.contactPoint].y) - 2) * internal.camera.zoom, 4 * internal.camera.zoom, 4 * internal.camera.zoom);
+                            internal.ctx.fillRect(obj.collider.vertexs[obj.collider.contactPoint].x - 2, (obj.collider.vertexs[obj.collider.contactPoint].y) - 2, 4, 4);
                         }
                     }
                 }
