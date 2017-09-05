23f5d3b1fcb343b9e7f7b66ac3a9e8685650df91
Driconmax <driconmax@gmail.com>
2017-09-05 17:34:21 -0300

    Added example

--- Javascript/src/extended.js
+++ Javascript/src/extended.js
@@ -11,3 +11,18 @@ function Color(r,g,b){
 Color.prototype.Lerp = function(a,b,l){
 	return new Color(Lerp(a.r, b.r, l), Lerp(a.g, b.g, l), Lerp(a.b, b.b, l));
 };
+
+
+/*
+
+var c1 = new Color(255, 255, 255);
+var c2 = new Color(0, 0, 0);
+
+var elem = document.getElementById("mai");
+var inter = setInterval(function(){
+	console.log(c2);
+	c2 = c1.Lerp(c1,c2,0.01);
+	elem.style.backgroundColor = "rgb("+Math.round(c2.r)+","+Math.round(c2.g)+","+Math.round(c2.b)+")";
+}, 100);
+
+ */
