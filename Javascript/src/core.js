db33e4175936f3ce24d156c5839b0f263704cd82
Driconmax <driconmax@gmail.com>
2017-09-12 15:04:29 -0300

    FT

--- Javascript/src/core.js
+++ Javascript/src/core.js
@@ -393,9 +393,9 @@ Collision Response - http://elancev.name/oliver/2D%20polygon.htm
             for (var i = 0; i < thread.msgTail.length; i++) {
                 if(thread.msgTail[i].cb != undefined){
                     thread.msgTail[i].id = internal.threads.msgId++;
-                    thread.cbTail[thread.msgTail[i].id] = thread.msgTail[i].cb;
+                    internal.threads.cbTail[thread.msgTail[i].id] = thread.msgTail[i].cb;
                 }
-                thread.obj.postMessage(thread.msgTail[i]);
+                thread.obj.postMessage(internal.threads.msgTail[i]);
             }
         }
 
