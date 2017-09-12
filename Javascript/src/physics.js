7f0409902f79781e532fe5904e9a65e4b7454765
Driconmax <driconmax@gmail.com>
2017-09-12 15:24:36 -0300

    FT

--- Javascript/src/physics.js
+++ Javascript/src/physics.js
@@ -21,7 +21,7 @@ var fixedLoop = {
 }
 
 onmessage = function(msg){
-    if(msg.data != undefined && typeof(msg.data) == Object){
+    if(msg.data != undefined && typeof(msg.data) == "object"){
         switch(msg.data.fn){
             case 'start':
                 phycs = msg.data.phycs;
